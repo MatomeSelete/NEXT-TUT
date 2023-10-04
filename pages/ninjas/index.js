a73633512bb33c58fd326b28bdbce7a294d0c98f
Matome <seletematome3@gmail.com>
2023-10-04 19:18:38 +0200

    dynamic routes part 2 done

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,29 +1,87 @@
-import Link from "next/link";
-import styles from "../../styles/Ninjas.module.css"
-
-
-export const getStaticProps = async () => {
+export const getStaticPaths = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
+    const data =  await res.json();
+
+    const paths = data.map(ninja =>{
+        return {
+            params: {id: ninja.id.toString() }
+        }
+    })
 
     return {
-        props: {ninjas: data }
+        paths,
+        fallback: false
+        // paths: [{ }, { }, {params: {id: }} ]
     }
 }
 
-const Ninjas = ({ninjas}) => {
+
+
+const Datails = () => {
     return ( 
         <div>
-            <h1>All Ninjas</h1>
-            {ninjas.map(ninja => 
-                <Link href={'./ninjas/' + ninja.id} key={ninja.id}>
-                    <h3 className={styles.single}>
-                        <h3>{ninja.name}</h3>
-                    </h3>
-                </Link>
-            )}
+            <h1>Details Page</h1>
+
         </div>
      );
 }
  
-export default Ninjas;
+export default Datails;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import Link from "next/link";
+// import styles from "../../styles/Ninjas.module.css"
+
+
+// export const getStaticProps = async () => {
+//     const res = await fetch('https://jsonplaceholder.typicode.com/users');
+//     const data = await res.json();
+
+//     return {
+//         props: {ninjas: data }
+//     }
+// }
+
+// const Ninjas = ({ninjas}) => {
+//     return ( 
+//         <div>
+//             <h1>All Ninjas</h1>
+//             {ninjas.map(ninja => 
+//                 <Link href={'./ninjas/' + ninja.id} key={ninja.id}>
+//                     <h3 className={styles.single}>
+//                         <h3>{ninja.name}</h3>
+//                     </h3>
+//                 </Link>
+//             )}
+//         </div>
+//      );
+// }
+ 
+// export default Ninjas;
