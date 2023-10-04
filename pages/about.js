72225ae7b45b8c92f0675fbc135719b4f8dec3bf
Matome <seletematome3@gmail.com>
2023-10-04 11:41:33 +0200

    images and metta data in next js

--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,23 @@
+import Head from "next/head";
+
 const About = () => {
-    return ( 
-        <div>
-            <h1> About </h1>
-            <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
-            <p> Lorem IpsuBm has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-            <p> Lorem IpsuBm has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-        </div>
-     );
+    return (
+        <>
+            <Head>
+                <title> Ninja List | About </title>
+                <meta name="keywords" content='ninjas' />
+            </Head>
+
+            <div>
+                <h1> About </h1>
+                <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+                <p> Lorem IpsuBm has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p> Lorem IpsuBm has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+            </div>
+
+        </>
+
+    );
 }
- 
+
 export default About;
--- pages/index.js
+++ pages/index.js
@@ -8,16 +8,21 @@ import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div >
+    <>
+      <Head>
+        <title> Ninja List | Home </title>
+        <meta name="keywords" content='ninjas' />
+      </Head>
 
-      <h1 className={styles.title}>HomePage</h1>
-      <p className={styles.text}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-      <p className={styles.text}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+      <div>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        <p className={styles.text}> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
 
-      <Link href="/ninjas">
-       <button className={styles.btn}> Ninja Listing</button> 
-      </Link>
-
-   </div>
+        <Link href="/ninjas">
+          <button className={styles.btn}> Ninja Listing</button>
+        </Link>
+      </div>
+    </>
   )
 }
